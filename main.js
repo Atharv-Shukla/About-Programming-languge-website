694528d612e78f940e27a5bd15e26ca73373f96a
Atharv Shukla <satharv0408@gmail.com>
2021-11-22 16:28:10 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+alert('This website is All Programming Languge and  Markup languge. If you want to know about programming languge click on programming languge button and if you want to know about HyperText Languge click on HyperText Languge button.   ')
+
+function About(){
+	document.write('Hello my name is Atharv Shukla I am junior software developer i make clone of websites like google. I had make this website using HTML, CSS, JAVASCRIPT  ')
+}
+function Credits(){
+	document.write('Programming - Atharv Shukla Design - Atharv Shukla Published on Github - Apoorva Shukla.')
+}
+function HyperTextLanguges(){
+	document.write('HTML Hypertext Markup Language. KML Keyhole Markup Language. MathML Mathematical Markup Language. SGML Standard Generalized Markup Language. XHTML eXtensible Hypertext Markup Language')
+}
+function Programminglanguges(){
+	document.write('C Java C++ Python C# Visual Basic JavaScript PHP  SQL Assembly language R Groovy ')
+}
+function AllStyleSheetLanguges(){
+	document.write('Cascading Style Sheets (CSS), Document Style Semantics and Specification Language (DSSSL), Extensible Stylesheet Language (XSL).')
+}
